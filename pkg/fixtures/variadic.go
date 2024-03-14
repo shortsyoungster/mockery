f7421af0ad39e108970c479423d522b041221d90
dajain <dajain@tesla.com>
2024-03-14 12:21:52 -0700

    Revert "Add test that asserts mocks on variadic return functions are behaving correctly"
    This reverts commit d119c66e267c739ff3dc9b0d1d55e5b8090f4e9c.

--- pkg/fixtures/variadic.go
+++ pkg/fixtures/variadic.go
@@ -6,3 +6,6 @@ type Variadic interface {
 	VariadicFunction(str string, vFunc VariadicFunction) error
 }
 
+type VariadicExtra interface {
+	SampleMethod(str string) func(str string, arr []int, a ...interface{})
+}
--- pkg/fixtures/variadic_return_func.go
+++
@@ -1,5 +0,0 @@
-package test
-
-type VariadicReturnFunc interface {
-	SampleMethod(str string) func(str string, arr []int, a ...interface{})
-}
